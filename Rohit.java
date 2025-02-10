ee477fa6b4d3de2c0327169320eef0933b60d81c
rohitlili <rohitkumawat110702@gmail.com>
2025-02-10 11:56:38 +0530

    First Commit

--- Rohit.java
+++ Rohit.java
@@ -14,7 +14,7 @@ int n = 5;
 System.out.println(fact(n));
 }
 }
-*/
+
 import java.util.*;
 public class Rohit{
 public static int power(int x, int n){
@@ -29,3 +29,38 @@ public static void main(String args[]){
 System.out.println(power(2, 2));
 }
 }
+*/
+public class Rohit{
+public static int calcSum(int n){
+if(n == 1){
+return 1;
+}
+int snm1 = calcSum(n-1);
+int sn = n+snm1;
+return sn;
+}
+public static void main(String args[]){
+int n = 5;
+System.out.println(calcSum(n));
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
